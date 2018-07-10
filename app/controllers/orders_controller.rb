08d41bc52c29daad968f732f8c7c40e8bbdaaf3f
hehehe2121 <hei5342hei@gmail.com>
2018-07-10 12:10:04 +0000

    add controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,21 @@
+class OrdersController < ApplicationController
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def thanks
+  end
+
+  end
+end

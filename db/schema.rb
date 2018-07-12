628189fd7f75faf659b10088581ec135887f0201
TokuyamaR <qpflapper.1007@gmail.com>
2018-07-12 08:46:34 +0000

    Modified db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_12_040509) do
+ActiveRecord::Schema.define(version: 2018_07_11_065940) do
 
   create_table "administrators", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,12 +29,28 @@ ActiveRecord::Schema.define(version: 2018_07_12_040509) do
     t.index ["reset_password_token"], name: "index_administrators_on_reset_password_token", unique: true
   end
 
+  create_table "addresses", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "postal_code", limit: 7, null: false
+    t.string "address", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "cart_items", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "product_id", null: false
     t.integer "quantity", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "greeds", force: :cascade do |t|
+    t.string "greed_type", null: false
+    t.string "greed_image_name_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["greed_type"], name: "index_greeds_on_greed_type"
   end
 
   create_table "musics", force: :cascade do |t|

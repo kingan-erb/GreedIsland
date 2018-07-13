df1aee006349ec8eb1f7d9cac3226e3a0a142ba8
fuchida64 <39880164+fuchida64@users.noreply.github.com>
2018-07-13 19:42:11 +0900

    Update music.rb

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,6 +1,5 @@
 class Music < ApplicationRecord
 	validates :music_name, presence: true
-	validates :music_number, presence: true
 	validates :disk_number, presence: true
 	validates :product_id, presence: true
 end

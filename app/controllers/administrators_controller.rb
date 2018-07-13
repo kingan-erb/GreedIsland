f2f25bdaeaa37c8ca2b6053764dc9f303108823d
fuchida64 <39880164+fuchida64@users.noreply.github.com>
2018-07-13 16:00:13 +0900

    Update administrators_controller.rb

--- app/controllers/administrators_controller.rb
+++ app/controllers/administrators_controller.rb
@@ -22,15 +22,15 @@ class AdministratorsController < ApplicationController
 	def update
 		@greed = Greed.find(params[:id])
 		@greed.update(greed_params)
-        redirect_to new_administrator_path
-        flash[:notice] = "更新されました"
+		redirect_to new_administrator_path
+		flash[:notice] = "更新されました"
 	end
 
 	def destroy
-    	@greed = Greed.find(params[:id])
-    	@greed.destroy
-    	redirect_to new_administrator_path
-      	flash[:notice] = "削除されました"
+		@greed = Greed.find(params[:id])
+		@greed.destroy
+		redirect_to new_administrator_path
+		flash[:notice] = "削除されました"
 	end
 
 	private

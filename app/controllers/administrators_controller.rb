bad181e926f517d15168fc6c7d474c28046234b6
fuchida64 <crbsfy731@gmail..com>
2018-07-15 07:24:47 +0000

    1

--- app/controllers/administrators_controller.rb
+++ app/controllers/administrators_controller.rb
@@ -1,5 +1,8 @@
 class AdministratorsController < ApplicationController
-
+	def new
+		@greed = Greed.new
+		@greeds = Greed.all
+	end
 	def index
 		if params[:cid]
 			@greed = Greed.find(params[:cid])
@@ -9,10 +12,34 @@ class AdministratorsController < ApplicationController
 		end
 	end
 
+	def create
+        @greed = Greed.new(greed_params)
+        @greed.save
+        redirect_to new_administrator_path
+  end
+
 	def show
 	end
 
 	def edit
 	end
 
+	def update
+		@greed = Greed.find(params[:id])
+		@greed.update(greed_params)
+		redirect_to new_administrator_path
+		flash[:notice] = "更新されました"
+	end
+
+	def destroy
+		@greed = Greed.find(params[:id])
+		@greed.destroy
+		redirect_to new_administrator_path
+		flash[:notice] = "削除されました"
+	end
+
+	private
+	def greed_params
+	    params.require(:greed).permit(:greed_type, :greed_image_name)
+	end
 end

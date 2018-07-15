7965eb818e4b5b0a6377680bc9b6ce44302c1952
TokuyamaR <qpflapper.1007@gmail.com>
2018-07-15 07:54:35 +0000

    add login logout後のリダイレクト先設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  	protected
+
+  	def configure_permitted_parameters
+    	devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :postcal_code,
+    													   :address, :phone_number, :payment_method, :greed_id])
+  	end
+
+	def after_sign_in_path_for(resource)
+	  	case resource
+	  	when User
+	  		greeds_index_path
+	  	when Administrator
+	  		greeds_index_path
+	  	end
+	end
+
+	def after_sign_out_path_for(resource)
+	  	case resource
+	  	when User
+	  		new_user_session_path
+	  	when Administrator
+	  		new_administrator_session_path
+	  	end
+	end
 
 end

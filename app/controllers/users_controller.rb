252eb1edf68285896c4275129fe27ea6082dba5b
fuchida64 <crbsfy731@gmail..com>
2018-07-18 05:30:48 +0000

    2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,29 +5,44 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(current_user.id)
 		@addresses = @user.addresses
+		@range = @user.addresses.length
 	end
 
 	def edit
+		@user = User.find(params[:id])
+		@addresses = @user.addresses
+		@addressnew = Address.new
 	end
 
 	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+		   redirect_to user_path(@user.id)
+		   flash[:notice] = "更新されました"
+		else
+		   redirect_to user_path(@user.id)
+		   flash[:alert] = "エラーが発生しました"
+		end
 	end
 
 
 	def destroy
 		@user = User.find(params[:id])
 		@user.soft_delete
-		#current_user
-		#sign_out(@user)
-		#else administrator
-		redirect_to admin_users_path
-        flash[:notice] = "削除されました"
-        #end
+		sign_out(@user)
+		if administrator_signed_in?
+			redirect_to admin_user_index_path
+			flash[:notice] = "削除されました"
+		else
+			redirect_to greeds_path
+			flash[:notice] = "退会しました"
+		end
 	end
 
 	def admin_index
 		@users = User.search(params[:search]).order(params[:sort]).order(id: :desc)
 		@navi = params[:sort]
+		@total = User.where(deleted_at: nil).length
 	end
 	def admin_show
 		@user = User.find(params[:id])
@@ -46,7 +61,7 @@ class UsersController < ApplicationController
 		   redirect_to admin_user_path(@user.id)
 		   flash[:notice] = "更新されました"
 		else
-		   redirect_to admin_user_index_path_path
+		   redirect_to admin_user_index_path
 		   flash[:alert] = "エラーが発生しました"
 		end
 	end

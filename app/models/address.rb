39d0d0be5eb713464d6a5a837c7badd552f4f3d3
fuchida64 <crbsfy731@gmail..com>
2018-07-20 04:08:55 +0000

    2

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,4 @@
 class Address < ApplicationRecord
 	belongs_to :user
 
-	validates :user_id, presence: true
-	validates :postal_code, presence: true, length: { maximum: 7 }
-	validates :address, presence: true
-
 end

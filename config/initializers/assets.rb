e44e12c71f09f749d63edce0b565882b19fdbe4a
fuchida64 <crbsfy731@gmail..com>
2018-07-25 07:17:05 +0000

    f_design_edit_product

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -37,6 +37,7 @@ Rails.application.config.assets.precompile += %w( products/index.scss )
 Rails.application.config.assets.precompile += %w( products/show.scss )
 Rails.application.config.assets.precompile += %w( products/admin_index.scss )
 Rails.application.config.assets.precompile += %w( products/admin_new.scss )
+Rails.application.config.assets.precompile += %w( products/admin_edit.scss )
 
 #orders
 Rails.application.config.assets.precompile += %w( orders/admin_index.scss )
